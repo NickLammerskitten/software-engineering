c7a71b0dbb456e35197e6cb9fc732426967e995e
Jan Bessler <jan.bessler@d-velop.de>
2024-11-18 16:33:02 +0100

    forgor again

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest } from "next/server";
 import { updateSession } from "./utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-    return updateSession(request);
+    return await updateSession(request);
 }
 
 export const config = {
