79ac0208c67bb868805baae931d8ba691a16af76
NickLammerskitten <120336345+NickLammerskitten@users.noreply.github.com>
2024-12-08 21:16:36 +0100

    feat(#7): Role check for add button

--- src/app/(pages)/gallery/page.tsx
+++ src/app/(pages)/gallery/page.tsx
@@ -1,7 +1,14 @@
+import { UserRole } from "@/src/app/models/userRole";
+import { createClient } from "@/src/utils/supabase/server";
 import { Add } from "@mui/icons-material";
 import { Button, Typography } from "@mui/material";
 
-export default function Gallery() {
+export default async function Gallery() {
+    const supabase = await createClient();
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
 
     return (
         <div>
@@ -9,11 +16,13 @@ export default function Gallery() {
                 Discover page
             </Typography>
 
-            <Button startIcon={<Add/>}
-                href="/gallery/add"
-            >
-                Bild hinzufügen
-            </Button>
+            {user?.role === UserRole.Trader && (
+                <Button startIcon={<Add/>}
+                    href="/gallery/add"
+                >
+                    Bild hinzufügen
+                </Button>
+            )}
         </div>
     );
 }
