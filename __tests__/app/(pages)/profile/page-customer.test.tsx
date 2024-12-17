2f52b0c9d0d256a51564015c6f796c707b1d2c00
NickLammerskitten <120336345+NickLammerskitten@users.noreply.github.com>
2024-12-17 13:38:44 +0100

    feat(#33): Tests

--- __tests__/app/(pages)/profile/page-customer.test.tsx
+++ __tests__/app/(pages)/profile/page-customer.test.tsx
@@ -2,7 +2,7 @@ import Profile from "@/src/app/(pages)/profile/page";
 import { render, screen } from "@testing-library/react";
 import { beforeEach, describe, expect, test, vi } from "vitest";
 
-describe('Settings Page for trader', () => {
+describe('Settings Page for customer', () => {
     beforeEach(() => {
         vi.mock('@/src/utils/supabase/server', () => ({
             createClient: vi.fn().mockImplementation(() => ({
@@ -11,11 +11,11 @@ describe('Settings Page for trader', () => {
                         data: {
                             user:
                                 {
-                                    id: '185f2f83-d63a-4c9b-b4a0-7e4a885799e2',
-                                    role: 'trader',
+                                    id: '185f2f83-d63a-4c9b-b4a0-7e4a885799e3',
+                                    role: 'authenticated',
                                     created_at: '2022-01-01T00:00:00.000000Z',
                                     updated_at: '2022-01-01T00:00:00.000000Z',
-                                    email: 'trader@email.com',
+                                    email: 'customer@email.com',
                                 },
                         },
                     }),
@@ -32,10 +32,10 @@ describe('Settings Page for trader', () => {
         expect(screen.getByRole('heading')).toBeDefined();
 
         expect(screen.findByText('E-Mail:')).toBeDefined();
-        expect(screen.findByText('trader@email.com')).toBeDefined();
+        expect(screen.findByText('customer@email.com')).toBeDefined();
 
         expect(screen.findByText('Rolle')).toBeDefined();
-        expect(screen.findByText('Händler')).toBeDefined();
+        expect(screen.findByText('Kunde')).toBeDefined();
 
         expect(screen.findByText('Erstellt am')).toBeDefined();
         expect(screen.findByText('01.01.2022')).toBeDefined();
@@ -44,6 +44,6 @@ describe('Settings Page for trader', () => {
         expect(screen.findByText('01.01.2022')).toBeDefined();
 
         expect(screen.findByText('ID')).toBeDefined();
-        expect(screen.findByText('185f2f83-d63a-4c9b-b4a0-7e4a885799e2')).toBeDefined();
+        expect(screen.findByText('185f2f83-d63a-4c9b-b4a0-7e4a885799e3')).toBeDefined();
     })
 });
