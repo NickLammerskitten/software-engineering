091a879f4be9f1acc1f0ace7bc2a049e44672567
NickLammerskitten <120336345+NickLammerskitten@users.noreply.github.com>
2024-12-18 10:34:14 +0100

    feat(#8): Add seeding and migration

--- supabase/migrations/20241217144127_image_path.sql
+++ supabase/migrations/20241217144127_image_path.sql
@@ -1,1 +1,44 @@
 alter table public.image add column image_path text;
+
+CREATE POLICY "Enable read access for authenticated users" ON "storage"."objects"
+    AS PERMISSIVE FOR SELECT
+    TO authenticated
+    USING (true);
+
+CREATE POLICY "Enable insert for trader users" ON "storage"."objects"
+    AS PERMISSIVE FOR INSERT
+    TO trader
+    WITH CHECK (true);
+
+CREATE POLICY "Enable update for trader user" ON "storage"."objects"
+    AS PERMISSIVE FOR UPDATE
+    TO trader
+    USING (true)
+    WITH CHECK (true);
+
+CREATE POLICY "Enable delete for trader user" ON "storage"."objects"
+    AS PERMISSIVE FOR DELETE
+    TO trader
+    USING (true);
+
+-- Policies for the "storage"."buckets" table
+CREATE POLICY "Enable read access for authenticated users" ON "storage"."buckets"
+    AS PERMISSIVE FOR SELECT
+    TO authenticated
+    USING (true);
+
+CREATE POLICY "Enable insert for trader user" ON "storage"."buckets"
+    AS PERMISSIVE FOR INSERT
+    TO trader
+    WITH CHECK (true);
+
+CREATE POLICY "Enable update for trader user" ON "storage"."buckets"
+    AS PERMISSIVE FOR UPDATE
+    TO trader
+    USING (true)
+    WITH CHECK (true);
+
+CREATE POLICY "Enable delete for trader user" ON "storage"."buckets"
+    AS PERMISSIVE FOR DELETE
+    TO trader
+    USING (true);
