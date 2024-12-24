8727be1764fa3681b94ec457c9d8f44ee98ebde5
NickLammerskitten <120336345+NickLammerskitten@users.noreply.github.com>
2024-12-24 15:03:27 +0100

    feat(#11): Separate route for customer portfolio

---
+++ src/app/(pages)/portfolio/my/page.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+export default async function MyPortfolioPage() {
+
+    return (
+        <div>
+            <Typography variant="h1">
+                Meine Auswahlmappe
+            </Typography>
+        </div>
+    )
+}
