232c46a37fcf4824430f61fe0e997a0f5ef06584
NickLammerskitten <120336345+NickLammerskitten@users.noreply.github.com>
2024-12-28 12:14:53 +0100

    feat(#11): implemented trader-managed portfolios (#57)
    Co-authored-by: Jan Bessler <64578396+Jaybee18@users.noreply.github.com>

--- src/app/(pages)/portfolio/page.tsx
+++ src/app/(pages)/portfolio/page.tsx
@@ -1,39 +1,24 @@
 import { PortfolioList } from "@/src/app/components/portfolio-list";
 import { TraderOnly } from "@/src/app/components/trader-only";
-import { UserRole } from "@/src/app/models/user-role";
-import { createClient } from "@/src/utils/supabase/server";
 import { Add } from "@mui/icons-material";
 import { Button, Typography } from "@mui/material";
 
 export default async function PortfolioPage() {
-    const supabase = await createClient();
-
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
-
     return (
-        <div>
-            {user?.role === UserRole.Trader ? (
-                <Typography variant={"h1"}>
-                    Themenmappen
-                </Typography>
-            ) : (
-                <Typography variant={"h1"}>
-                    Auswahlmappen
-                </Typography>
-            )}
+        <TraderOnly>
+            <Typography variant={"h1"}>
+                Themenmappen
+            </Typography>
 
-            <TraderOnly>
-                <Button startIcon={<Add />}
-                        href="/portfolio/add"
-                        className={"top_action_buttons"}
-                >
-                    Hinzufügen
-                </Button>
-            </TraderOnly>
+            <Button
+                startIcon={<Add />}
+                href="/portfolio/add"
+                className={"top_action_buttons"}
+            >
+                Hinzufügen
+            </Button>
 
             <PortfolioList />
-        </div>
+        </TraderOnly>
     );
 }
