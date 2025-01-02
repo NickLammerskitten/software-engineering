84b883724b69f102afe1e673043cf350b299784e
Florian Sander <129592840+Flo-is-coding@users.noreply.github.com>
2025-01-02 17:39:08 +0100

    feat(#72): added back button on image detail view (#73)

--- src/app/(pages)/image/[id]/page.tsx
+++ src/app/(pages)/image/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { DetailedImage } from "@/src/app/components/detailed-image";
 import { UserRole } from "@/src/app/models/user-role";
 import { createClient } from "@/src/utils/supabase/server";
+import {Box, Button} from "@mui/material";
+import {ArrowBackIosNew} from "@mui/icons-material";
 
 export default async function DetailPage() {
     const supabaseClient = await createClient();
@@ -11,6 +13,9 @@ export default async function DetailPage() {
 
     return (
         <>
+            <Box>
+                <Button href="/gallery" startIcon={<ArrowBackIosNew/>}>Zurück</Button>
+            </Box>
             {user && (
                 <DetailedImage isTrader={user.role === UserRole.Trader} />
             )}
