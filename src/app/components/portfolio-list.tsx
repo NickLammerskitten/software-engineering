98b9765418ff2b83e6bde68ac4f86d29b986b2ec
NickLammerskitten <120336345+NickLammerskitten@users.noreply.github.com>
2025-01-16 22:51:14 +0100

    feat(#61): Remove unused import

--- src/app/components/portfolio-list.tsx
+++ src/app/components/portfolio-list.tsx
@@ -3,7 +3,7 @@
 import { Portfolio } from "@/src/app/models/portfolio";
 import { useConfirmDialog } from "@/src/app/utils/confirm-dialog-hook";
 import { Delete, Edit } from "@mui/icons-material";
-import { Alert, Box, Card, CircularProgress, IconButton, Typography } from "@mui/material";
+import { Alert, Box, Card, CircularProgress, IconButton } from "@mui/material";
 import { useSnackbar } from "notistack";
 import { Fragment, useEffect, useState } from "react";
 
